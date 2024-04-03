2186ffc9c87655b87a331149a426f811ef38eec9
Tejas Tanna <tannatejas@gmail.com>
2024-04-03 14:18:05 +0100

    initialize default values using sql file, Adding required properties and configuration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into ASSET_TB(id, asset_id, price)
+values(1, 'S1', 50.5);
+insert into ASSET_TB(id, asset_id, price)
+values(2, 'S2', 20.2);
+insert into ASSET_TB(id, asset_id, price)
+values(3, 'S3', 10.4);
+insert into ASSET_TB(id,asset_id, price)
+values(4, 'S4', 15.5);
